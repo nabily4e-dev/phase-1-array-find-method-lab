4839b363a46ffce69ea7379440cc1798a711affa
nabily4e-dev <nbl.dev21@gmail.com>
2023-01-01 16:57:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    for (let i = 0; i < record.length; i++) {
+      if (record[i].result === 'W') {
+        return record[i].year;
+      }
+    }
+    return undefined;
+  }
+  
